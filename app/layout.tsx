d08003b4a15006a6e0bacd192aac1abfd988027d
pipeleon <oflg94@gmail.com>
2024-06-17 15:59:56 -0500

    adding accesibility button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const proximaNovaAltFont = localFont({
   variable: "--font-proxima-nova-a",
@@ -85,6 +86,58 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${proximaNovaAltFont.variable}`}>
+      <Script
+        id="show-accesibility-widget"
+        dangerouslySetInnerHTML={{
+          __html: `
+          <!-- Accessibility Code for "www.mas60datosplateados.com" -->
+          /*
+
+          Want to customize your button? visit our documentation page:
+
+          https://login.equalweb.com/custom-button.taf
+
+          */
+          window.interdeal = {
+              "sitekey": "427badf766cb21a4adc344eba97ee8cb",
+              "Position": "left",
+              "domains": {
+                  "js": "https://cdn.equalweb.com/",
+                  "acc": "https://access.equalweb.com/"
+              },
+              "Menulang": "es-MX",
+              "draggable": false,
+              "btnStyle": {
+                  "vPosition": [
+                      "50%",
+                      "20%"
+                  ],
+                  "scale": [
+                      "0.5",
+                      "0.5"
+                  ],
+                  "color": {
+                      "main": "#1d5556",
+                      "second": "#ffffff"
+                  },
+                  "icon": {
+                      "type": 11,
+                      "shape": "circle"
+                  }
+              }
+          };
+          (function(doc, head, body){
+              var coreCall             = doc.createElement('script');
+              coreCall.src             = interdeal.domains.js + 'core/4.6.14/accessibility.js';
+              coreCall.defer           = true;
+              coreCall.integrity       = 'sha512-DeZBWLOEHK1CkmU3vQTNSXeENfZmhTHFmn7zzhvZCPERahpp/DTouMc1ZHOu+RAo1FTKnuKEf1NQyB76z8mU1Q==';
+              coreCall.crossOrigin     = 'anonymous';
+              coreCall.setAttribute('data-cfasync', true );
+              body? body.appendChild(coreCall) : head.appendChild(coreCall);
+          })(document, document.head, document.body);
+          `,
+        }}
+      />
       <body>{children}</body>
     </html>
   );
