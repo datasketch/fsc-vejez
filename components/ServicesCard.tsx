f1f760c8af5bba7e3531c16ac283bb63f52552b2
David Daza <03dazal@gmail.com>
2024-06-20 10:57:09 -0500

    DSPRJT-6468: Connect services data hosted on Datasketch
    Co-authored-by: pipeleon <oflg94@gmail.com>

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 interface ServicesCardProps {
-  type: string;
-  country: string;
-  title: string;
-  description: string;
-  externalLink?: string;
+  categoria: string;
+  pais_de_origen: string;
+  nombre: string;
+  descripcion_del_servicio: string;
+  pagina_web?: string;
 }
 
-export const typeImages: any = {
+export const CATEGORY_ICONS: any = {
   "Salud y bienestar": "/images/servicios/bienestar.svg",
   Educación: "/images/servicios/educacion.svg",
   "Ingresos y finanzas": "/images/servicios/finanzas.svg",
@@ -17,27 +17,27 @@ export const typeImages: any = {
 };
 
 export default function ServicesCard({
-  type,
-  country,
-  title,
-  description,
-  externalLink,
+  categoria,
+  pais_de_origen,
+  nombre,
+  descripcion_del_servicio,
+  pagina_web,
 }: ServicesCardProps) {
   return (
     <div className="bg-seasalt rounded-[10px] p-5 u-shadow-1">
       <div className="flex gap-x-5">
         <div className="flex-shrink-0">
-          <Image src={typeImages[type]} alt="" width={60} height={60} />
+          <Image src={CATEGORY_ICONS[categoria]} alt="" width={60} height={60} />
         </div>
         <div className="w-full">
-          <p className="text-lg lg:text-xl italic">{type}</p>
-          <p>{country}</p>
-          <h3 className="mt-2.5 text-lg lg:text-xl font-semibold">{title}</h3>
-          <p>{description}</p>
-          {externalLink && (
+          <p className="text-lg lg:text-xl italic">{categoria}</p>
+          <p>{pais_de_origen}</p>
+          <h3 className="mt-2.5 text-lg lg:text-xl font-semibold">{nombre}</h3>
+          <p>{descripcion_del_servicio}</p>
+          {pagina_web && (
             <Link
               className="mt-6 inline-flex py-2 px-5 gap-x-2 text-dark-slate-gray font-semibold bg-asparagus/40 rounded-[20px]"
-              href={externalLink}
+              href={pagina_web}
               target="_blank"
             >
               <p className="text-nowrap">Ir a la página</p>
